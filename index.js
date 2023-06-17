a8b72e836a001016b2a97f92e06b5bee6775524a
Paul <you@example.com>
2023-06-17 18:25:05 +0300

    add server log

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 
 const errorMiddleware = require('./middleware/error');
+const loggerMiddleware = require('./middleware/logger');
 
 const indexRouter = require('./routes/index');
 const bookApiRouter = require('./routes/api/book');
@@ -13,6 +14,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.set("view engine", "ejs");
+app.use(loggerMiddleware);
+
 
 app.use('/', indexRouter);
 app.use('/book', bookRouter);
